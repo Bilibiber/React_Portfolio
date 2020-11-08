71c3bbe756c56567e8995892aba2896f7ea70f0b
Fan Guo <guofx@dukes.jmu.edu>
2020-11-08 10:07:01 -0500

    dependenices and new images

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Home from "./HomePage/Home"
 import CodePen from "./CodePen/CodePen"
 import TodoList from "./TodoList/TodoList"
 import MuiNav from "./Mui-todo/MuiNav"
+import Netflix from "./Netflix/Netflix"
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
+          <Redirect exact from="/" to="/Netflix" />
           <Route path="/home" exact component={Home} />
           <Route path="/codePen" exact component={CodePen} />
           <Route path="/TodoList" exact component={TodoList} />
+          <Route path="/Netflix" exact component={Netflix} />
           <Redirect exact from="/MuiTodo" to="/MuiTodo/main" />
           <Route path="/MuiTodo/:page?" exact render={(props) => <MuiNav {...props} />} />
         </Switch>
