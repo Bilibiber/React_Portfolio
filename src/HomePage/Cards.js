eca61ea6d0c7f3bcec177dfcfb0546b439b290e0
Fan Guo <guofx@dukes.jmu.edu>
2020-11-20 13:52:57 -0500

    added some styles to home page

--- src/HomePage/Cards.js
+++ src/HomePage/Cards.js
@@ -4,14 +4,17 @@ import './Cards.css'
 
 function Cards() {
   return (
-    <div className='cards' id='demo'>
+    <div className='cards'>
       <h1>CHECK OUT ALL MY DEMOS</h1>
       <div className='cards-container'>
         <div className='cards-wrapper'>
-          <ul className='cards-items'>
-            <CardItems src='../images/CodePen.jpg' text='A live web development editor, enabled HTML, CSS and JavaScript' label='CodePen' path='/codePen' />
+          <ul className='cards-items' id='demo'>
             <CardItems src='../images/netflix-logo.png' text='An Netflix website colone' label='Netflix' path='/Netflix' />
             <CardItems src='../images/pikachu.jpg' text='Pokemon fan? Check this out' label='Pokemon List' path='/Pokemon' />
+          </ul>
+          <ul className='cards-items'>
+            <CardItems src='../images/CodePen.jpg' text='A live web development editor, enabled HTML, CSS and JavaScript' label='CodePen' path='/codePen' />
+            <CardItems src='../images/coming-soon5.png' label='KuaKuaAI' text=' having a rough day? Come here to relax' path='/ECommerce' />
             <CardItems src='../images/coming-soon5.png' text='Developing' label='E-Commerce' path='/ECommerce' />
           </ul>
         </div>
